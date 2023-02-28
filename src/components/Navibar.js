0d7efcd213926e2f970648cc1fc21b2660ff263a
Timur <timengalytchev@mail.ru>
2023-02-28 12:09:11 +0300

    fix

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -31,22 +31,22 @@ const NavigationBar = () => {
             <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link><Link to="/">Home</Link></Nav.Link>
-                    <Nav.Link><Link to="/users">Users</Link></Nav.Link>
-                    <Nav.Link><Link to="/about">About</Link></Nav.Link>
+                    <Nav.Link><Link to="/">Главная</Link></Nav.Link>
+                    <Nav.Link><Link to="/users">Каталог</Link></Nav.Link>
+                    <Nav.Link><Link to="/about">О нас</Link></Nav.Link>
                 </Nav>
                 
             </Navbar.Collapse>
             <Nav className="justify-content-end ">
-                    <Button variant="primary" className="me-2" onClick={handleShow}>Log In</Button>
-                    <Button variant="primary" className="me-2" onClick={handleShow}>Log Out</Button>
+                    <Button variant="primary" className="me-2" onClick={handleShow}>Заказать звонок</Button>
+                    
             </Nav>
             </Container>
         </Navbar>
         </Styles>
         <Modal show={show} onHide={handleShow}>
             <Modal.Header closeButton onClick={handleClose}>
-                <Modal.Title>Log In</Modal.Title>
+                <Modal.Title>Мы свяжемся с Вами!</Modal.Title>
             </Modal.Header>
 
 
@@ -54,18 +54,18 @@ const NavigationBar = () => {
                 <Form>
 
                     <Form.Group controlId="fromBasicEmail">
-                        <Form.Label>Email adress</Form.Label>
+                        <Form.Label>Введите Email</Form.Label>
                         <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">We'll never share your email</Form.Text>
+                        <Form.Text className="text-muted">Мы не будем присылать рекламу на Вашу почту</Form.Text>
                     </Form.Group>
                     <Form.Group controlId="fromBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Enter password" />
+                        <Form.Label>Номер телефона</Form.Label>
+                        <Form.Control type="tel" placeholder="+7-900-000-00-00" />
                     </Form.Group>
                     <Form.Group controlId="fromBasicCheckbox">
                         <Form.Check type="checkbox" label="Remember me" />
                     </Form.Group>
-
+                    <Button variant="primary" className="me-2" onClick={handleClose}>Отправить</Button>
                 </Form>
             </Modal.Body>
         </Modal>
